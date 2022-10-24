a544c98df57241064954e9f9022052eb16a1edcc
charith1997 <charithmadu97@gmail.com>
2022-10-24 08:43:50 +0530

    UI implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./App.css";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1 style={{ paddingLeft: "5%" }}>Meal Shop</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} />
